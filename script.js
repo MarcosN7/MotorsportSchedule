f903d9c61669459a670b237131abbad520ac02d5
Marcos Bastos <51135929+MarcosN7@users.noreply.github.com>
2023-05-24 12:41:04 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,534 @@
+const scheduleData = {
+    formula1: [
+        {
+            event: "BAHRAIN GP",
+            date: "MAR 05",
+            time: "11:00"
+          },
+          {
+            event: "SAUDI ARABIAN GP",
+            date: "MAR 19",
+            time: "13:00"
+          },
+          {
+            event: "AUSTRALIAN GP",
+            date: "APR 02",
+            time: "01:00"
+          },
+          {
+            event: "AZERBAIJAN GP",
+            date: "APR 30",
+            time: "07:00"
+          },
+          {
+            event: "MIAMI GP",
+            date: "MAY 07",
+            time: "15:30"
+          },
+          {
+            event: "EMILIA ROMAGNA GP",
+            date: "MAY 21",
+            time: "CANCELED"
+          },
+          {
+            event: "MONACO GP",
+            date: "MAY 28",
+            time: "09:00"
+          },
+          {
+            event: "SPANISH GP",
+            date: "JUN 04",
+            time: "09:00"
+          },
+          {
+            event: "CANADIAN GP",
+            date: "JUN 18",
+            time: "14:00"
+          },
+          {
+            event: "AUSTRIAN GP",
+            date: "JUL 02",
+            time: "09:00"
+          },
+          {
+            event: "BRITISH GP",
+            date: "JUL 09",
+            time: "10:00"
+          },
+          {
+            event: "HUNGARIAN GP",
+            date: "JUL 23",
+            time: "09:00"
+          },
+          {
+            event: "BELGIAN GP",
+            date: "JUL 30",
+            time: "09:00"
+          },
+          {
+            event: "DUTCH GP",
+            date: "AUG 27",
+            time: "09:00"
+          },
+          {
+            event: "ITALIAN GP",
+            date: "SEP 03",
+            time: "09:00"
+          },
+          {
+            event: "SINGAPORE GP",
+            date: "SEP 17",
+            time: "08:00"
+          },
+          {
+            event: "JAPANESE GP",
+            date: "SEP 24",
+            time: "01:00"
+          },
+          {
+            event: "QATAR GP",
+            date: "OCT 08",
+            time: "10:00"
+          },
+          {
+            event: "UNITED STATES GP",
+            date: "OCT 22",
+            time: "15:00"
+          },
+          {
+            event: "MEXICO CITY GP",
+            date: "OCT 29",
+            time: "16:00"
+          },
+          {
+            event: "BRAZILIAN GP",
+            date: "NOV 05",
+            time: "13:00"
+          },
+          {
+            event: "LAS VEGAS GP",
+            date: "NOV 19",
+            time: "02:00"
+          },
+          {
+            event: "ABU DHABI GP",
+            date: "NOV 26",
+            time: "09:00"
+          }
+    ],
+    nascar: [
+      {
+        date: 'SUN | FEB 5',
+        event: 'CLASH AT THE COLISEUM',
+        time: '8 PM',
+        network: 'FOX'
+      },
+      {
+        date: 'THU | FEB 16',
+        event: 'DUEL AT DAYTONA',
+        time: '7 PM',
+        network: 'FS1'
+      },
+      {
+        date: 'SUN | FEB 19',
+        event: 'DAYTONA 500',
+        time: '2:30 PM',
+        network: 'FOX'
+      },
+      {
+        date: 'SUN | FEB 26',
+        event: 'AUTO CLUB',
+        time: '3:30 PM',
+        network: 'FOX'
+      },
+      {
+        date: 'SUN | MAR 5',
+        event: 'LAS VEGAS',
+        time: '3:30 PM',
+        network: 'FOX'
+      },
+      {
+        date: 'SUN | MAR 12',
+        event: 'PHOENIX',
+        time: '3:30 PM',
+        network: 'FOX'
+      },
+      {
+        date: 'SUN | MAR 19',
+        event: 'ATLANTA',
+        time: '3 PM',
+        network: 'FOX'
+      },
+      {
+        date: 'SUN | MAR 26',
+        event: 'COTA',
+        time: '3:30 PM',
+        network: 'FOX'
+      },
+      {
+        date: 'SUN | APR 2',
+        event: 'RICHMOND',
+        time: '3:30 PM',
+        network: 'FS1'
+      },
+      {
+        date: 'SUN | APR 9',
+        event: 'BRISTOL DIRT',
+        time: '7 PM',
+        network: 'FOX'
+      },
+      {
+        date: 'SUN | APR 16',
+        event: 'MARTINSVILLE',
+        time: '3 PM',
+        network: 'FS1'
+      },
+      {
+        date: 'SUN | APR 23',
+        event: 'TALLADEGA',
+        time: '3 PM',
+        network: 'FOX'
+      },
+      {
+        date: 'SUN | APR 30',
+        event: 'DOVER',
+        time: '2 PM',
+        network: 'FS1'
+      },
+      {
+        date: 'SUN | MAY 7',
+        event: 'KANSAS',
+        time: '3 PM',
+        network: 'FS1'
+      },
+      {
+        date: 'SUN | MAY 14',
+        event: 'DARLINGTON',
+        time: '3 PM',
+        network: 'FS1'
+      },
+      {
+        date: 'SUN | MAY 21',
+        event: 'NORTH WILKESBORO (ASR)',
+        time: '8 PM',
+        network: 'FS1'
+      },
+      {
+        date: 'SUN | MAY 28',
+        event: 'CHARLOTTE',
+        time: '6 PM',
+        network: 'FOX'
+      },
+      {
+        date: 'SUN | JUN 4',
+        event: 'WWTR',
+        time: '3:30 PM',
+        network: 'FS1'
+      },
+      {
+        date: 'SUN | JUN 11',
+        event: 'SONOMA',
+        time: '3:30 PM',
+        network: 'FOX'
+      },
+      {
+        date: 'SUN | JUN 25',
+        event: 'NASHVILLE',
+        time: '7 PM',
+        network: 'NBC'
+      },
+      {
+        date: 'SUN | JUL 2',
+        event: 'CHICAGO STREET RACE',
+        time: '5:30 PM',
+        network: 'NBC'
+      },
+      {
+        date: 'SUN | JUL 9',
+        event: 'ATLANTA',
+        time: '7 PM',
+        network: 'USA'
+      },
+      {
+        date: 'SUN | JUL 16',
+        event: 'NEW HAMPSHIRE',
+        time: '2:30 PM',
+        network: 'USA'
+      },
+      {
+        date: 'SUN | JUL 23',
+        event: 'POCONO',
+        time: '2:30 PM',
+        network: 'USA'
+      },
+      {
+        date: 'SUN | JUL 30',
+        event: 'RICHMOND',
+        time: '3 PM',
+        network: 'USA'
+      },
+      {
+        date: 'SUN | AUG 6',
+        event: 'MICHIGAN',
+        time: '2:30 PM',
+        network: 'USA'
+      },
+      {
+        date: 'SUN | AUG 13',
+        event: 'INDIANAPOLIS RC',
+        time: '2:30 PM',
+        network: 'NBC'
+      },
+      {
+        date: 'SUN | AUG 20',
+        event: 'WATKINS GLEN',
+        time: '3:00 PM',
+        network: 'USA'
+      },
+      {
+        date: 'SAT | AUG 26',
+        event: 'DAYTONA',
+        time: '7:00 PM',
+        network: 'NBC'
+      },
+      {
+        date: 'SUN | SEP 3',
+        event: 'DARLINGTON',
+        time: '6:00 PM',
+        network: 'USA'
+      },
+      {
+        date: 'SUN | SEP 10',
+        event: 'KANSAS',
+        time: '3:00 PM',
+        network: 'USA'
+      },
+      {
+        date: 'SAT | SEP 16',
+        event: 'BRISTOL',
+        time: '7:30 PM',
+        network: 'USA'
+      },
+      {
+        date: 'SUN | SEP 24',
+        event: 'TEXAS',
+        time: '3:30 PM',
+        network: 'USA'
+      },
+      {
+        date: 'SUN | OCT 1',
+        event: 'TALLADEGA',
+        time: '2:00 PM',
+        network: 'NBC'
+      },
+      {
+        date: 'SUN | OCT 8',
+        event: 'CHARLOTTE ROVAL',
+        time: '2:00 PM',
+        network: 'NBC'
+      },
+      {
+        date: 'SUN | OCT 15',
+        event: 'LAS VEGAS',
+        time: '2:30 PM',
+        network: 'NBC'
+      },
+      {
+        date: 'SUN | OCT 22',
+        event: 'HOMESTEAD-MIAMI',
+        time: '2:30 PM',
+        network: 'NBC'
+      },
+      {
+        date: 'SUN | OCT 29',
+        event: 'MARTINSVILLE',
+        time: '2:00 PM',
+        network: 'NBC'
+      },
+      {
+        date: 'SUN | NOV 5',
+        event: 'PHOENIX',
+        time: '3:00 PM',
+        network: 'NBC'
+      }
+    ],
+    motogp: [
+        {
+            event: "SEPANG FEBRUARY TESTING",
+            date: "FEB 09",
+            time: "22:00"
+          },
+          {
+            event: "PORTIMAO MARCH TESTING",
+            date: "MAR 11",
+            time: "06:00"
+          },
+          {
+            event: "PORTUGAL GP",
+            date: "MAR 26",
+            time: "09:00"
+          },
+          {
+            event: "ARGENTINIAN GP",
+            date: "APR 02",
+            time: "13:00"
+          },
+          {
+            event: "AMERICAS GP",
+            date: "APR 16",
+            time: "15:00"
+          },
+          {
+            event: "SPANISH GP",
+            date: "APR 30",
+            time: "09:00"
+          },
+          {
+            event: "FRENCH GP",
+            date: "MAY 14",
+            time: "08:00"
+          },
+          {
+            event: "ITALIAN GP",
+            date: "JUN 11",
+            time: "08:00"
+          },
+          {
+            event: "GERMAN GP",
+            date: "JUN 18",
+            time: "08:00"
+          },
+          {
+            event: "DUTCH GP",
+            date: "JUN 25",
+            time: "08:00"
+          },
+          {
+            event: "BRITISH GP",
+            date: "AUG 06",
+            time: "08:00"
+          },
+          {
+            event: "AUSTRIAN GP",
+            date: "AUG 20",
+            time: "08:00"
+          },
+          {
+            event: "CATALAN GP",
+            date: "SEP 03",
+            time: "08:00"
+          },
+          {
+            event: "SAN MARINO GP",
+            date: "SEP 10",
+            time: "08:00"
+          },
+          {
+            event: "INDIAN GP",
+            date: "SEP 24",
+            time: "04:30"
+          },
+          {
+            event: "JAPANESE GP",
+            date: "OCT 01",
+            time: "01:00"
+          },
+          {
+            event: "INDONESIAN GP",
+            date: "OCT 15",
+            time: "02:00"
+          },
+          {
+            event: "AUSTRALIAN GP",
+            date: "OCT 21",
+            time: "23:00"
+          },
+          {
+            event: "THAILAND GP",
+            date: "OCT 29",
+            time: "03:00"
+          },
+          {
+            event: "MALAYSIAN GP",
+            date: "NOV 12",
+            time: "02:00"
+          },
+          {
+            event: "QATAR GP",
+            date: "NOV 19",
+            time: "13:00"
+          },
+          {
+            event: "VALENCIA GP",
+            date: "NOV 26",
+            time: "09:00"
+          }
+    ],
+}
+
+function populateSchedule(category) {
+    const scheduleContainer = document.getElementById('schedule-container');
+    scheduleContainer.innerHTML = ''; // Clear previous schedule
+  
+    const schedule = scheduleData[category];
+  
+    if (!schedule || schedule.length === 0) {
+      scheduleContainer.innerHTML = '<p>No events found for this category.</p>';
+      return;
+    }
+  
+    schedule.forEach(item => {
+      const scheduleItem = document.createElement('div');
+      scheduleItem.classList.add('schedule-item');
+  
+      const logo = document.createElement('img');
+  
+      // Set the logo based on the category
+      switch (category) {
+        case 'motogp':
+          logo.src = 'motogp.png';
+          break;
+        case 'f1':
+          logo.src = 'f1.png';
+          break;
+        case 'nascar':
+          logo.src = 'NASCAR.png';
+          break;
+        // Add more cases for other categories as needed
+        default:
+          logo.src = 'f1.png';
+          break;
+      }
+  
+      // You can also set additional attributes or styles for the logo if needed
+      scheduleItem.appendChild(logo);
+  
+      const eventDetails = document.createElement('div');
+      eventDetails.classList.add('event-details');
+      eventDetails.innerHTML = `
+        <strong>Date:</strong> ${item.date}<br>
+        <strong>Event:</strong> ${item.event}<br>
+        <strong>Time:</strong> ${item.time}<br>
+        <strong>Network:</strong> ${item.network}
+      `;
+  
+      scheduleItem.appendChild(eventDetails);
+      scheduleContainer.appendChild(scheduleItem);
+    });
+  }
+  
+  
+  // Event listener for category selection change
+  const categorySelect = document.getElementById('category-select');
+  categorySelect.addEventListener('change', function() {
+    const selectedCategory = this.value;
+    populateSchedule(selectedCategory);
+  });
+  
+  // Initial population of schedule based on default category
+  const defaultCategory = categorySelect.value;
+  populateSchedule(defaultCategory);
+  
+  // ...
+
+  
